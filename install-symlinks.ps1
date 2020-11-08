8a6b94c319bd59ec78a6869268c24525b1b4903d
nalsai <contact@nalsai.de>
2020-11-08 17:16:14 +0100

    fix symlinks

--- install-symlinks.ps1
+++ install-symlinks.ps1
@@ -7,24 +7,24 @@
 Write-Host "Making Symlinks..." -ForegroundColor Green
 
 # Windows Terminal config
-New-Item -Force -ItemType SymbolicLink $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\ -LocalState settings.json -Value $PSScriptRoot\WindowsTerminal
+New-Item -Force -ItemType SymbolicLink -Path $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState -Target $PSScriptRoot\WindowsTerminal
 
 # PowerShell
 $documents = [Environment]::GetFolderPath("MyDocuments")
-New-Item -Force -ItemType SymbolicLink $documents\PowerShell -LocalState Microsoft.PowerShell_profile.ps1 -Value $PSScriptRoot\PowerShell
-New-Item -Force -ItemType SymbolicLink $documents\WindowsPowerShell -LocalState Microsoft.PowerShell_profile.ps1 -Value $PSScriptRoot\PowerShell
+New-Item -Force -ItemType SymbolicLink -Path $documents\PowerShell -Target $PSScriptRoot\PowerShell
+New-Item -Force -ItemType SymbolicLink -Path $documents\WindowsPowerShell -Target $PSScriptRoot\PowerShell
 
 # mpv config
-New-Item -Force -ItemType SymbolicLink $HOME\AppData\Roaming\ -Name mpv -Value $PSScriptRoot\mpv
+New-Item -Force -ItemType SymbolicLink -Path $HOME\AppData\Roaming\ -Target $PSScriptRoot\mpv
 
 # Visual Studio Code settings.json and keybindings.json
-New-Item -Force -ItemType SymbolicLink $HOME\AppData\Roaming\Code\User\ -Name settings.json -Value $PSScriptRoot\vscode\settings.json
-New-Item -Force -ItemType SymbolicLink $HOME\AppData\Roaming\Code\User\ -Name keybindings.json -Value $PSScriptRoot\vscode\keybindings.json
+New-Item -Force -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User\settings.json -Target $PSScriptRoot\vscode\settings.json
+New-Item -Force -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User\keybindings.json -Target $PSScriptRoot\vscode\keybindings.json
 
 # .gitconfig
-New-Item -Force -ItemType SymbolicLink $HOME\ -Name .gitconfig -Value $PSScriptRoot\git\.gitconfig
+New-Item -Force -ItemType SymbolicLink -Path $HOME\.gitconfig -Target $PSScriptRoot\git\.gitconfig
 
 # AutoHotkey
-New-Item -Force -ItemType SymbolicLink $env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\ -Name myAhk.ahk -Value $PSScriptRoot\ahk\myAhk.ahk
+New-Item -Force -ItemType SymbolicLink -Path $env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\myAhk.ahk -Target $PSScriptRoot\ahk\myAhk.ahk
 
 Write-Host "Done Making Symlinks" -ForegroundColor Green

817816a281e5104ca1898aea4578d7758361917b
nalsai <contact@nalsai.de>
2020-11-14 21:35:25 +0100

    default to install things

--- full-install.ps1
+++ full-install.ps1
@@ -2,28 +2,32 @@
 
 Set-ExecutionPolicy RemoteSigned
 
-Write-Host "Install symlinks? (y/N): " -ForegroundColor Yellow -NoNewline
+Write-Host "Install symlinks? (Y/n): " -ForegroundColor Yellow -NoNewline
 Switch (Read-Host)
 {
-    Y {Invoke-Expression -Command "$PSScriptRoot\install-symlinks.ps1"}
+    Default {Invoke-Expression -Command "$PSScriptRoot\install-symlinks.ps1"}
+    N {}
 }
 
-Write-Host "Install settings? (y/N): " -ForegroundColor Yellow -NoNewline
+Write-Host "Install settings? (Y/n): " -ForegroundColor Yellow -NoNewline
 Switch (Read-Host)
 {
-    Y {Invoke-Expression -Command "$PSScriptRoot\install-settings.ps1"}
+    Default {Invoke-Expression -Command "$PSScriptRoot\install-settings.ps1"}
+    N {}
 }
 
-Write-Host "Install apps? (y/N): " -ForegroundColor Yellow -NoNewline
+Write-Host "Install apps? (Y/n): " -ForegroundColor Yellow -NoNewline
 Switch (Read-Host)
 {
-    Y {Invoke-Expression -Command "$PSScriptRoot\install-apps.ps1"}
+    Default {Invoke-Expression -Command "$PSScriptRoot\install-apps.ps1"}
+    N {}
 }
 
-Write-Host "Install fonts? (y/N): " -ForegroundColor Yellow -NoNewline
+Write-Host "Install fonts? (Y/n): " -ForegroundColor Yellow -NoNewline
 Switch (Read-Host)
 {
-    Y {Invoke-Expression -Command "$PSScriptRoot\install-fonts.ps1"}
+    Default {Invoke-Expression -Command "$PSScriptRoot\install-fonts.ps1"}
+    N {}
 }
 
 Write-Host "Done! Note that some changes require a restart to take effect." -ForegroundColor Green

939850da16394a152ce92f85df319502b6ba27d0
nalsai <contact@nalsai.de>
2020-11-14 21:33:11 +0100

    improve performance

--- install-apps.ps1
+++ install-apps.ps1
@@ -7,66 +7,79 @@
 Write-Host "Installing Apps..." -ForegroundColor Green
 
 ### Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco feature enable -n=allowGlobalConfirmation
-& $profile  # Refresh Profile
+if ($null -eq (which cinst)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    & $profile
+    choco feature enable -n=allowGlobalConfirmation
+}
 
 choco install git --params '"/GitAndUnixToolsOnPath /NoShellIntegration"' --limit-output
 choco install googlechrome nomacs spotify autohotkey curl wget --ignore-checksums --limit-output
+Start-Process choco -ArgumentList "install powershell-core --install-arguments='`"ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1`"' --limit-output"
+Start-Process choco -ArgumentList "install mp3tag --package-parameters='`"/NoDesktopShortcut /NoContextMenu`"' --limit-output"
+Start-Process choco -ArgumentList "install plex golang hugo python openjdk unity-hub mpv ffmpeg youtube-dl mkvtoolnix aegisub subtitleedit makemkv mediainfo mediainfo-cli eac audacity audacity-lame cdburnerxp burnawarefree etcher obs-studio openssl.light filezilla windirstat libreoffice-fresh exiftool flacsquisher authy-desktop paint.net linkshellextension image-composite-editor icaros figma discord deluge renamer 7zip wireshark winmerge --ignore-checksums --limit-output"
 
 refreshenv
 & $profile
 
-Write-Host "Please open Spotify and close it again, then press enter" -ForegroundColor Yellow -NoNewline
-Read-Host
-
 # Customize Spotify using spicetify
+if (!(Test-Path $HOME\spicetify)) {
+    Start-Process $env:APPDATA\Spotify\Spotify.exe
+    Start-Sleep 3
+    Stop-Process -Name Spotify
 
-choco install spicetify-cli --limit-output
-spicetify
-spicetify backup enable-devtool
-Set-Location $HOME
-git clone https://github.com/morpheusthewhite/spicetify-themes
-Set-Location spicetify-themes
-Copy-Item -r * $HOME\.spicetify\Themes\ -ErrorAction Ignore
-Set-Location $HOME
-Remove-Item .\spicetify-themes\ -Recurse -Force
-spicetify config current_theme CherryBlossom
-git clone https://github.com/khanhas/spicetify-cli
-Set-Location .\spicetify-cli\Extensions\
-Copy-Item -r * $HOME\.spicetify\Extensions\ -ErrorAction Ignore
-Set-Location $HOME
-Remove-Item .\spicetify-cli\ -Recurse -Force
-spicetify config extensions fullAppDisplay.js 
-spicetify config extensions keyboardShortcut.js
-spicetify config extensions shuffle+.js
-spicetify apply
+    choco install spicetify-cli --limit-output
+    spicetify
+    spicetify backup enable-devtool
+    Set-Location $HOME
+    git clone https://github.com/morpheusthewhite/spicetify-themes
+    Set-Location spicetify-themes
+    Copy-Item -r * $HOME\.spicetify\Themes\ -ErrorAction Ignore
+    Set-Location $HOME
+    Remove-Item .\spicetify-themes\ -Recurse -Force
+    spicetify config current_theme CherryBlossom
+    git clone https://github.com/khanhas/spicetify-cli
+    Set-Location .\spicetify-cli\Extensions\
+    Copy-Item -r * $HOME\.spicetify\Extensions\ -ErrorAction Ignore
+    Set-Location $HOME
+    Remove-Item .\spicetify-cli\ -Recurse -Force
+    spicetify config extensions fullAppDisplay.js 
+    spicetify config extensions keyboardShortcut.js
+    spicetify config extensions shuffle+.js
+    spicetify apply
+}
 
-
-Write-Host "Installing .NET 3.5"
-Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3"
-
-choco install powershell-core --install-arguments='"ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1"' --limit-output
-choco install winmerge --install-arguments='"/NoShellIntegration"' --limit-output
-choco install mp3tag --package-parameters='"/NoDesktopShortcut /NoContextMenu"' --limit-output
-choco install plex golang hugo python openjdk unity-hub mpv ffmpeg youtube-dl mkvtoolnix aegisub subtitleedit makemkv mediainfo mediainfo-cli eac audacity audacity-lame cdburnerxp burnawarefree etcher obs-studio openssl.light filezilla windirstat libreoffice-fresh exiftool flacsquisher authy-desktop paint.net linkshellextension image-composite-editor icaros figma discord deluge renamer 7zip wireshark --ignore-checksums --limit-output
+if(!((Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name 'Version' -ErrorAction SilentlyContinue | ForEach-Object {$_.Version -as [System.Version]} | Where-Object {$_.Major -eq 3 -and $_.Minor -eq 5}).Count -ge 1)) {
+    Write-Host "Installing .NET 3.5"
+    Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3"
+}
 
 # prevent choco upgrade of automatically upgrading packages that upgrade themselves
-choco pin add --name googlechrome
-choco pin add --name unity-hub
-choco pin add --name authy-desktop
-choco pin add --name figma
-choco pin add --name discord
-choco pin add --name spotify
+$pin_block = {
+    $apps =
+    'googlechrome',
+    'unity-hub',
+    'authy-desktop',
+    'figma',
+    'discord',
+    'spotify'
+    for ($i = 0; $i -lt $apps.Count; $i++) {
+        choco pin add --name $apps[$i]
+    } 
+}
+Start-Process powershell -ArgumentList "-command $pin_block"
 
-#################
-# Install VS Code
-#################
 Start-Process wget -ArgumentList "-O VSCode-Setup.exe https://aka.ms/win32-x64-user-stable" -WorkingDirectory $HOME -Wait
 .\VSCode-Setup.exe /VERYSILENT /MERGETASKS=!runcode /NoDesktopIcon /NoQuicklaunchIcon /NoAddContextMenuFiles /AssociateWithFiles
 
-refreshenv
-& $profile
+Start-Process wget -ArgumentList "-O ImgReName-Setup.exe https://nalsai.de/imgrename/download/Setup.exe" -WorkingDirectory $HOME -Wait
+.\ImgReName-Setup.exe --silent
+
+Start-Process wget -ArgumentList "-O CrystalDiskInfo-Setup.exe https://crystalmark.info/redirect.php?product=CrystalDiskInfoInstallerShizuku" -WorkingDirectory $HOME -Wait
+.\CrystalDiskInfo-Setup.exe /SP- /VERYSILENT /NORESTART
+
+Start-Process wget -ArgumentList "-O CrystalDiskMark-Setup.exe https://crystalmark.info/redirect.php?product=CrystalDiskMarkInstallerShizuku" -WorkingDirectory $HOME -Wait
+.\CrystalDiskMark-Setup.exe /SP- /VERYSILENT /NORESTART
 
 $installation_block = {
     $extensions =
@@ -87,21 +100,12 @@ $installation_block = {
     'Yummygum.city-lights-icon-vsc',
     'yummygum.city-lights-theme',
     'Zignd.html-css-class-completion'
-
     for ($i = 0; $i -lt $extensions.Count; $i++) {
         code --install-extension $extensions[$i]
     } 
 }
 Start-Process powershell -ArgumentList "-command $installation_block"   # Invoke new poweshell instance so code is in path
 
-Start-Process wget -ArgumentList "-O CrystalDiskInfo-Setup.exe https://crystalmark.info/redirect.php?product=CrystalDiskInfoInstallerShizuku" -WorkingDirectory $HOME -Wait
-.\CrystalDiskInfo-Setup.exe /SP- /VERYSILENT /NORESTART
-
-Start-Process wget -ArgumentList "-O CrystalDiskMark-Setup.exe https://crystalmark.info/redirect.php?product=CrystalDiskMarkInstallerShizuku" -WorkingDirectory $HOME -Wait
-.\CrystalDiskMark-Setup.exe /SP- /VERYSILENT /NORESTART
-
-Start-Process wget -ArgumentList "-O ImgReName-Setup.exe https://nalsai.de/imgrename/download/Setup.exe" -WorkingDirectory $HOME -Wait
-.\ImgReName-Setup.exe --silent
 
 Write-Host "Install itunes? (y/N): " -ForegroundColor Yellow -NoNewline
 Switch (Read-Host) 
@@ -135,6 +139,7 @@ Switch (Read-Host)
 } 
 
 # Remove remaining setup files
+Remove-Item VSCode-Setup.exe
 Remove-Item ImgReName-Setup.exe
 Remove-Item CrystalDiskInfo-Setup.exe
 Remove-Item CrystalDiskMark-Setup.exe

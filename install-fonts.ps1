642e45ff7e108854b4d6a4fbb028b0954f03416b
nalsai <contact@nalsai.de>
2020-11-15 21:07:39 +0100

    fix font installation

--- install-fonts.ps1
+++ install-fonts.ps1
@@ -33,7 +33,6 @@ foreach($font in $fonts)
     Remove-Item $fontsPath\$font.zip -Force
 }
 Remove-Item "$fontsPath\static\" -Recurse -Force
-
 Expand-Archive -Path "$fontsPath\Cascadia Code.zip" -DestinationPath $fontsPath -Force
 Remove-Item "$fontsPath\ttf\static\" -Recurse -Force
 Copy-Item -r  $fontsPath\ttf\* $fontsPath -ErrorAction Ignore
@@ -42,18 +41,14 @@ Remove-Item "$fontsPath\otf\" -Recurse -Force
 Remove-Item "$fontsPath\woff2\" -Recurse -Force
 Remove-Item "$fontsPath\ttf\" -Recurse -Force
 
-# install font files
-foreach($file in $(Get-ChildItem -Path $fontsPath -Include ('*.otf','*.ttf') -Recurse)){
-    If (!(Test-Path "c:\windows\fonts\$($File.name)"))
+foreach($File in $(Get-ChildItem -Path $fontsPath -Include ('*.otf','*.ttf') -Recurse)){
+    If (!(Test-Path "C:\Windows\Fonts\$($File.Name)"))
     {
-        $(New-Object -ComObject Shell.Application).Namespace(0x14).CopyHere($file.fullname,  4 + 16);
-        New-ItemProperty -Name $file.fullname -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $file 
+        Copy-Item $File.FullName C:\Windows\Fonts\$($File.Name)
+        New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -Name $File.Name -PropertyType string -Value $File.Name
     }
 }
-
-Start-Sleep 3
 
 Remove-Item $fontsPath -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Done Installing Fonts" -ForegroundColor Green
-Write-Host "The folder $HOME\Downloads\Fonts may still be left and can be deleted." -ForegroundColor Cyan

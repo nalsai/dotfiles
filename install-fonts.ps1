abfde81f5f3a0531c309e911a2aae38c52e721e8
nalsai <contact@nalsai.de>
2020-11-19 21:47:00 +0100

    improve font installation

--- install-fonts.ps1
+++ install-fonts.ps1
@@ -25,6 +25,7 @@ foreach($font in $fonts)
 {
     wget -O $fontsPath\$font.zip "https://fonts.google.com/download?family=$font"
 }
+
 wget -O "$fontsPath\Cascadia Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/microsoft/cascadia-code/releases/latest").assets | Where-Object name -like CascadiaCode-*.zip ).browser_download_url
 wget -O "$fontsPath\Gandhi Sans.zip" https://www.fontsquirrel.com/fonts/download/gandhi-sans
 
@@ -33,18 +34,21 @@ foreach($font in $fonts)
     Expand-Archive -Path $fontsPath\$font.zip -DestinationPath $fontsPath -Force
     Remove-Item $fontsPath\$font.zip -Force
 }
-Remove-Item "$fontsPath\static\" -Recurse -Force
+
 Expand-Archive -Path "$fontsPath\Cascadia Code.zip" -DestinationPath $fontsPath -Force
 Expand-Archive -Path "$fontsPath\Gandhi Sans.zip" -DestinationPath $fontsPath -Force
+Remove-Item "$fontsPath\Cascadia Code.zip" -Force
+Remove-Item "$fontsPath\Gandhi Sans.zip" -Force
 Remove-Item "$fontsPath\ttf\static\" -Recurse -Force
 Copy-Item -r  $fontsPath\ttf\* $fontsPath -ErrorAction Ignore
-Remove-Item "$fontsPath\Cascadia Code.zip" -Force
-Remove-Item "$fontsPath\otf\" -Recurse -Force
-Remove-Item "$fontsPath\woff2\" -Recurse -Force
-Remove-Item "$fontsPath\ttf\" -Recurse -Force
+Remove-Item "$fontsPath\otf" -Recurse -Force
+Remove-Item "$fontsPath\static" -Recurse -Force
+Remove-Item "$fontsPath\ttf" -Recurse -Force
+Remove-Item "$fontsPath\woff2" -Recurse -Force
+Remove-Item "$fontsPath\*.txt" -Force
 
 foreach($File in $(Get-ChildItem -Path $fontsPath -Include ('*.otf','*.ttf') -Recurse)){
-    If (!(Test-Path "C:\Windows\Fonts\$($File.Name)"))
+    if (!(Test-Path "C:\Windows\Fonts\$($File.Name)"))
     {
         Copy-Item $File.FullName C:\Windows\Fonts\$($File.Name)
         New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -Name $File.Name -PropertyType string -Value $File.Name

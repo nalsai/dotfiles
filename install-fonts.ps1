0994e4d351d7ab565f189979a7ad56e7e5a34c95
nalsai <contact@nalsai.de>
2020-12-11 17:58:46 +0100

    add Fira Code font

--- install-fonts.ps1
+++ install-fonts.ps1
@@ -26,8 +26,14 @@ foreach ($font in $fonts) {
 	wget.exe -O $fontsPath\$font.zip "https://fonts.google.com/download?family=$font"
 }
 
-wget.exe -O "$fontsPath\Cascadia Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/microsoft/cascadia-code/releases/latest").assets | Where-Object name -like CascadiaCode-*.zip ).browser_download_url
+wget.exe -O "$fontsPath\Fira Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/tonsky/FiraCode/releases/latest").assets | Where-Object name -like Fira*.zip ).browser_download_url
+wget.exe -O "$fontsPath\Cascadia Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/microsoft/cascadia-code/releases/latest").assets | Where-Object name -like Cascadia*.zip ).browser_download_url
 wget.exe -O "$fontsPath\Gandhi Sans.zip" https://www.fontsquirrel.com/fonts/download/gandhi-sans
+
+Expand-Archive -Path "$fontsPath\Fira Code.zip" -DestinationPath $fontsPath -Force
+Copy-Item -r  $fontsPath\variable_ttf\* $fontsPath -ErrorAction SilentlyContinue
+Remove-Item "$fontsPath\ttf" -Recurse -Force
+Remove-Item "$fontsPath\Fira Code.zip" -Force
 
 foreach ($font in $fonts) {
 	Expand-Archive -Path $fontsPath\$font.zip -DestinationPath $fontsPath -Force

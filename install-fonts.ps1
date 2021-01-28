e59dd72d9ae0bf7e5ce0483fedb23dd0f7d3754a
nalsai <contact@nalsai.de>
2021-01-28 13:49:35 +0100

    fix fonts & add some more to minimal-install

--- install-fonts.ps1
+++ install-fonts.ps1
@@ -24,37 +24,26 @@ $fonts =
 
 foreach ($font in $fonts) {
 	wget.exe -O $fontsPath\$font.zip "https://fonts.google.com/download?family=$font"
+	Expand-Archive -Path $fontsPath\$font.zip -DestinationPath $fontsPath -Force
 }
+Remove-Item "$fontsPath\static\" -Recurse -Force
+
+wget.exe -O "$fontsPath\Gandhi Sans.zip" https://www.fontsquirrel.com/fonts/download/gandhi-sans
+Expand-Archive -Path "$fontsPath\Gandhi Sans.zip" -DestinationPath $fontsPath -Force
 
 wget.exe -O "$fontsPath\Fira Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/tonsky/FiraCode/releases/latest").assets | Where-Object name -like Fira*.zip ).browser_download_url
+Expand-Archive -Path "$fontsPath\Fira Code.zip" -DestinationPath "$fontsPath\Fira Code" -Force
+Copy-Item -r "$fontsPath\Fira Code\variable_ttf\*" $fontsPath -ErrorAction SilentlyContinue
+Remove-Item "$fontsPath\Fira Code" -Recurse -Force
+
 wget.exe -O "$fontsPath\Cascadia Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/microsoft/cascadia-code/releases/latest").assets | Where-Object name -like Cascadia*.zip ).browser_download_url
-wget.exe -O "$fontsPath\Gandhi Sans.zip" https://www.fontsquirrel.com/fonts/download/gandhi-sans
+Expand-Archive -Path "$fontsPath\Cascadia Code.zip" -DestinationPath "$fontsPath\Cascadia Code" -Force
+Remove-Item "$fontsPath\Cascadia Code\ttf\static" -Recurse -Force
+Copy-Item -r  "$fontsPath\Cascadia Code\ttf\*" $fontsPath -ErrorAction SilentlyContinue
+Remove-Item "$fontsPath\Cascadia Code" -Recurse -Force
 
-Expand-Archive -Path "$fontsPath\Fira Code.zip" -DestinationPath $fontsPath -Force
-Copy-Item -r  $fontsPath\variable_ttf\* $fontsPath -ErrorAction SilentlyContinue
-Remove-Item "$fontsPath\variable_ttf" -Recurse -Force
-Remove-Item "$fontsPath\ttf" -Recurse -Force
-Remove-Item "$fontsPath\Fira Code.zip" -Force
-
-foreach ($font in $fonts) {
-	Expand-Archive -Path $fontsPath\$font.zip -DestinationPath $fontsPath -Force
-	Remove-Item $fontsPath\$font.zip -Force
-}
-
-Expand-Archive -Path "$fontsPath\Cascadia Code.zip" -DestinationPath $fontsPath -Force
-Expand-Archive -Path "$fontsPath\Gandhi Sans.zip" -DestinationPath $fontsPath -Force
-Remove-Item "$fontsPath\Cascadia Code.zip" -Force
-Remove-Item "$fontsPath\Gandhi Sans.zip" -Force
-Remove-Item "$fontsPath\ttf\static\" -Recurse -Force
-Copy-Item -r  $fontsPath\ttf\* $fontsPath -ErrorAction SilentlyContinue
-Remove-Item "$fontsPath\otf" -Recurse -Force
-Remove-Item "$fontsPath\static" -Recurse -Force
-Remove-Item "$fontsPath\ttf" -Recurse -Force
-Remove-Item "$fontsPath\woff" -Recurse -Force
-Remove-Item "$fontsPath\woff2" -Recurse -Force
 Remove-Item "$fontsPath\*.txt" -Force
-Remove-Item "$fontsPath\*.html" -Force
-Remove-Item "$fontsPath\*.css" -Force
+Remove-Item "$fontsPath\*.zip" -Force
 
 foreach ($File in $(Get-ChildItem -Path $fontsPath -Include ('*.otf', '*.ttf') -Recurse)) {
 	if (!(Test-Path "C:\Windows\Fonts\$($File.Name)")) {
--- minimal-install.ps1
+++ minimal-install.ps1
@@ -248,14 +248,23 @@ $fonts =
 'Noto Serif'
 foreach ($font in $fonts) {
 	wget.exe -O $fontsPath\$font.zip "https://fonts.google.com/download?family=$font"
-}
-
-foreach ($font in $fonts) {
 	Expand-Archive -Path $fontsPath\$font.zip -DestinationPath $fontsPath -Force
-	Remove-Item $fontsPath\$font.zip -Force
 }
-
 Remove-Item "$fontsPath\static\" -Recurse -Force
+
+wget.exe -O "$fontsPath\Gandhi Sans.zip" https://www.fontsquirrel.com/fonts/download/gandhi-sans
+Expand-Archive -Path "$fontsPath\Gandhi Sans.zip" -DestinationPath $fontsPath -Force
+
+wget.exe -O "$fontsPath\Fira Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/tonsky/FiraCode/releases/latest").assets | Where-Object name -like Fira*.zip ).browser_download_url
+Expand-Archive -Path "$fontsPath\Fira Code.zip" -DestinationPath "$fontsPath\Fira Code" -Force
+Copy-Item -r "$fontsPath\Fira Code\variable_ttf\*" $fontsPath -ErrorAction SilentlyContinue
+Remove-Item "$fontsPath\Fira Code" -Recurse -Force
+
+wget.exe -O "$fontsPath\Cascadia Code.zip" ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/microsoft/cascadia-code/releases/latest").assets | Where-Object name -like Cascadia*.zip ).browser_download_url
+Expand-Archive -Path "$fontsPath\Cascadia Code.zip" -DestinationPath "$fontsPath\Cascadia Code" -Force
+Remove-Item "$fontsPath\Cascadia Code\ttf\static" -Recurse -Force
+Copy-Item -r  "$fontsPath\Cascadia Code\ttf\*" $fontsPath -ErrorAction SilentlyContinue
+Remove-Item "$fontsPath\Cascadia Code" -Recurse -Force
 
 foreach ($File in $(Get-ChildItem -Path $fontsPath -Include ('*.otf', '*.ttf') -Recurse)) {
 	if (!(Test-Path "C:\Windows\Fonts\$($File.Name)")) {

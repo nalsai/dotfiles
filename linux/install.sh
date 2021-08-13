e2c15a17c203f795e76f46f830af4d611a38b077
Nils Fürniß <contact@nalsai.de>
2021-08-13 23:11:17 +0200

    added mpv stuff

--- linux/install.sh
+++ linux/install.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
 
+
+
+
+if chrome is installed:
+linux/chrome/dark_mode.sh
+
+
+Symlinks:
+
+vscode --> ~/.config/Code/User           (Files)
+vscode --> ~/.config/code-oss/User       (Files)
+vscode --> ~/.config/Code - OSS/User     (Files)
+
+mpv -->  ~/.config/mpv
+
+git/.gitconfig -->  ~/.gitconfig
+
+
+
+
+mpv:
+
+/home/nalsai/.config/plex-mpv-shim
+
+/home/nalsai/.var/app/com.github.iwalton3.jellyfin-mpv-shim/config/jellyfin-mpv-shim
+
+/home/nalsai/.var/app/io.mpv.Mpv/config/mpv
+/home/nalsai/.config/mpv
+
+permissions
+flatpak: allow access to screenshot path
+~/Pictures/mpv-screenshots:create
+
+ask for sponsorblock.txt

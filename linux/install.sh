ec3686977864e540ebbbcc4f1041fd9cebc9549a
Nils Fürniß <contact@nalsai.de>
2021-09-28 20:31:09 +0200

    (linux) made splashscreen & yes/no prompt

--- linux/install.sh
+++ linux/install.sh
@@ -1,61 +1,67 @@
 #!/bin/bash
+clear
+echo -e " _   _ _ _     ____        _    __ _ _\n| \ | (_| |___|  _ \  ___ | |_ / _(_| | ___ ___\n|  \| | | / __| | | |/ _ \| __| |_| | |/ _ / __|\n| |\  | | \__ | |_| | (_) | |_|  _| | |  __\__ \\n|_| \_|_|_|___|____/ \___/ \__|_| |_|_|\___|___/"
+echo
+echo "This script is still work in progress and currently only made for Fedora."
+
+echo -n "Continue? [y/n]: "
+# https://stackoverflow.com/questions/226703/how-do-i-prompt-for-yes-no-cancel-input-in-a-linux-shell-script#27875395
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+    echo y
+else
+    echo n
+fi
+
+# TODO:
+# [1] minimal installation
+# [2] full installation
+# [3] server installation
 
 
-
-Server installation
-minimal installation
-full installation
+# download dotfiles
 
 
----
+# make symlinks
+#ln -sf 
+
+#git/.gitconfig -->  ~/.gitconfig
+#vscode --> ~/.config/Code/User
+#vscode --> ~/.config/code-oss/User
+#vscode --> ~/.config/Code - OSS/User
+#/home/nalsai/.config/plex-mpv-shim
+#/home/nalsai/.var/app/com.github.iwalton3.jellyfin-mpv-shim/config/jellyfin-mpv-shim
+#/home/nalsai/.var/app/io.mpv.Mpv/config/mpv
+#/home/nalsai/.config/mpv
+#celluloid
 
 
-git/.gitconfig -->  ~/.gitconfig
-ask for gpg key 
-    else remove gpg from config
+# change os settings
 
 
-if chrome is installed:
-linux/chrome/dark_mode.sh
+# install apps
 
 
-Symlinks:
-
-vscode --> ~/.config/Code/User
-vscode --> ~/.config/code-oss/User
-vscode --> ~/.config/Code - OSS/User
-
-
-mpv:
-
-/home/nalsai/.config/plex-mpv-shim
-
-/home/nalsai/.var/app/com.github.iwalton3.jellyfin-mpv-shim/config/jellyfin-mpv-shim
-
-/home/nalsai/.var/app/io.mpv.Mpv/config/mpv
-/home/nalsai/.config/mpv
-
-# celluloid
-
-permissions
-flatpak: allow access to screenshot path
-~/Pictures/mpv-screenshots:create
-
-ask for sponsorblock.txt
+# change app settings?
+#if chrome is installed:
+#linux/chrome/dark_mode.sh
+#git gpg key
+#flatpak: allow access to screenshot path
+#~/Pictures/mpv-screenshots:create
+#mpv ask for sponsorblock.txt
+#gnome terminal
 
 
-
-gnome terminal
-
-fish
-
-nvim
+# install fonts
 
 
-Apps:
-
-dnf:
-
-pacman/yay:
-
-flatpaks:
+# other TODO:
+#nvim
+#fish
+#Apps:
+#dnf:
+#pacman/yay:
+#flatpaks:

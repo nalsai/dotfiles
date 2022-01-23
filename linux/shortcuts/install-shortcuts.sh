f5c0de623716a878df5d9ad9c0819c5fa16f8db5
Nils Fürniß <contact@nalsai.de>
2022-01-23 15:13:06 +0100

    (linux) add Melty Blood symlink

--- linux/shortcuts/install-shortcuts.sh
+++ linux/shortcuts/install-shortcuts.sh
@@ -9,4 +9,4 @@ ln -s $DOT/linux/shortcuts/tsukihime.desktop $HOME/.local/share/applications/
 # Folders
 ln -s "/home/nalsai/.local/share/Steam/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/Documents/Elder Scrolls Online" "/home/nalsai/Documents/Elder Scrolls Online"
 ln -s "/home/nalsai/.var/app/com.usebottles.bottles/data/bottles/bottles/Fate/drive_c/users/carrie/documents/faterealtanua_savedata" "/home/nalsai/Apps/Bottles/Fate Stay Night/faterealtanua_savedata"
-
+ln -s "/home/nalsai/Sync/Files/Documents/Melty Blood: Type Lumina Save" "/home/nalsai/.local/share/Steam/steamapps/common/MELTY BLOOD TYPE LUMINA/winsave/228783925"

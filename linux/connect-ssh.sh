fff1e8eacc2a388b4f6919cfaf8adbafb520f129
Nils Fürniß <contact@nalsai.de>
2022-02-19 20:26:43 +0100

    update ssh list

--- linux/connect-ssh.sh
+++ linux/connect-ssh.sh
@@ -1,13 +1,13 @@
 echo 'Please select the server' 
 
-select s in "hachiman.nils.moe" "shiina.nils.moe" "vps.nils.moe" "minecraft.nils.moe" "pi@raspberrypi" "nalsai@nanopineo3" "nalsai@odroidxu4" "nalsai@nanopi-r4s" "138.3.248.255 (OpenMPTCProuter)"; do
+select s in "hachiman.nils.moe" "shiina.nils.moe" "vps.nils.moe" "minecraft.nils.moe" "pi@raspberrypi" "nalsai@odroidxu4" "nalsai@nanopineo3" "nalsai@nanopi-r4s" "138.3.248.255 (OpenMPTCProuter)"; do
   case $s in
   "hachiman.nils.moe")
     ssh root@hachiman.nils.moe -p 2022
     break
     ;;
   "shiina.nils.moe")
-    ssh root@shiina.nils.moe
+    ssh root@shiina.nils.moe -p 2022
     break
     ;;
   "vps.nils.moe")
@@ -22,12 +22,12 @@ select s in "hachiman.nils.moe" "shiina.nils.moe" "vps.nils.moe" "minecraft.nils
     ssh pi@raspberrypi
     break
     ;;
-  "nalsai@nanopineo3")
-    ssh nalsai@nanopineo3
-    break
-    ;;
   "nalsai@odroidxu4")
     ssh nalsai@odroidxu4
+    break
+    ;;
+  "nalsai@nanopineo3")
+    ssh nalsai@nanopineo3
     break
     ;;
   "nalsai@nanopi-r4s")

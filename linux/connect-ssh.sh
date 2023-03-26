6eaaa1e2dc8a23c3685818ac525b534740968038
Nils Fürniß <contact@nalsai.de>
2023-03-26 23:17:26 +0200

    Update ssh urls

--- linux/connect-ssh.sh
+++ linux/connect-ssh.sh
@@ -5,19 +5,19 @@ echo 'Please select the server:'
 select s in "hachiman.nils.moe" "shiina.nils.moe" "shion.nils.moe" "odroidxu4.nils.moe"; do
   case $s in
   "hachiman.nils.moe")
-    ssh root@hachiman.nils.moe -p 2022
+    ssh root@ssh.hachiman.nils.moe -p 2022
     break
     ;;
   "shiina.nils.moe")
-    ssh root@shiina.nils.moe -p 2022
+    ssh root@ssh.shiina.nils.moe -p 2022
     break
     ;;
   "shion.nils.moe")
-    ssh opc@shion.nils.moe
+    ssh opc@ssh.shion.nils.moe
     break
     ;;
   "odroidxu4.nils.moe")
-    ssh root@odroidxu4.nils.moe -p 2223
+    ssh root@ssh.odroidxu4.nils.moe -p 2223
     break
     ;;
   *)

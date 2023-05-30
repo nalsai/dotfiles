a060878c31f9a61630f7e985752614d8619c96b1
Nils Fürniß <contact@nalsai.de>
2023-05-30 20:50:59 +0200

    Add missing -y to distrobox-fedora-resolve.sh

--- linux/scripts/distrobox-fedora-resolve.sh
+++ linux/scripts/distrobox-fedora-resolve.sh
@@ -5,11 +5,11 @@ if [ -f /setup_done ]; then
 fi
 
 echo "Installing dependencies..."
-sudo dnf install alsa-lib apr apr-util fontconfig freetype libglvnd-egl librsvg2 libXcursor libXi libXinerama libxkbcommon-x11 libXrandr libXrender libXtst mtdev pulseaudio-libs
-sudo dnf install mesa-libGLU xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
+sudo dnf -y install alsa-lib apr apr-util fontconfig freetype libglvnd-egl librsvg2 libXcursor libXi libXinerama libxkbcommon-x11 libXrandr libXrender libXtst mtdev pulseaudio-libs
+sudo dnf -y install mesa-libGLU xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
 
 echo "Installing runtime dependencies..."
-sudo dnf install alsa-plugins-pulseaudio libxcrypt-compat
-sudo dnf install rocm-opencl  # AMD GPU
+sudo dnf -y install alsa-plugins-pulseaudio libxcrypt-compat
+sudo dnf -y install rocm-opencl  # AMD GPU
 
 touch /setup_done

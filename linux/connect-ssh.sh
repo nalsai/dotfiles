8765b9d47b7dbc9a03adddee99cb560f4b500d95
Nils Fürniß <contact@nalsai.de>
2026-07-03 21:49:59 +0200

    Update fish and add tide

--- linux/connect-ssh.sh
+++ linux/connect-ssh.sh
@@ -2,18 +2,14 @@
 
 echo 'Please select the server:'
 
-select s in "hachiman.nils.moe" "alya.nils.moe" "odroidxu4.nils.moe"; do
+select s in "hachiman.nils.moe" "alya.nils.moe"; do
   case $s in
   "hachiman.nils.moe")
-    ssh root@hachiman -p 2022 || ssh root@hachiman.nils.moe -p 2022
+    ssh root@hachiman -p 2022   # ssh root@hachiman.nils.moe -p 2022
     break
     ;;
   "alya.nils.moe")
-    ssh root@alya || ssh root@alya.nils.moe
-    break
-    ;;
-  "odroidxu4.nils.moe")
-    ssh root@odroidxu4 -p 2223
+    ssh root@alya   # ssh root@alya.nils.moe
     break
     ;;
   *)
